52ba58b842866a5dc47c836244e02888b00e7572
salaheddineva <salaheddine@veniseactivation.com>
2024-06-13 16:23:30 +0100

    Fix: actions disabled states

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,15 +11,15 @@
 </head>
 
 <body>
-  <div class="flex flex-col items-center justify-center h-screen w-full gap-y-4 p-8">
-    <video class="bg-slate-100 rounded-3xl w-48 h-36" id="localVideo" playsinline autoplay muted></video>
-    <video class="bg-slate-100 rounded-3xl w-full max-w-5xl h-3/4 object-cover" id="remoteVideo" playsinline autoplay></video>
+  <div class="flex flex-col items-center justify-center w-full h-screen p-8 gap-y-4">
+    <video class="w-48 bg-slate-100 rounded-3xl h-36" id="localVideo" playsinline autoplay muted></video>
+    <video class="object-cover w-full max-w-5xl bg-slate-100 rounded-3xl h-3/4" id="remoteVideo" playsinline autoplay></video>
 
-    <div class="border w-full max-w-5xl py-3 px-4 rounded-3xl flex items-center justify-center gap-x-4">
-      <button class="text-white bg-green-500 py-2 px-4 rounded-full" id="startButton">
+    <div class="flex items-center justify-center w-full max-w-5xl px-4 py-3 border rounded-3xl gap-x-4">
+      <button class="px-4 py-2 text-white bg-green-500 rounded-full disabled:opacity-50" id="startButton">
         <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="currentColor" d="M391 480c-19.52 0-46.94-7.06-88-30c-49.93-28-88.55-53.85-138.21-103.38C116.91 298.77 93.61 267.79 61 208.45c-36.84-67-30.56-102.12-23.54-117.13C45.82 73.38 58.16 62.65 74.11 52a176.3 176.3 0 0 1 28.64-15.2c1-.43 1.93-.84 2.76-1.21c4.95-2.23 12.45-5.6 21.95-2c6.34 2.38 12 7.25 20.86 16c18.17 17.92 43 57.83 52.16 77.43c6.15 13.21 10.22 21.93 10.23 31.71c0 11.45-5.76 20.28-12.75 29.81c-1.31 1.79-2.61 3.5-3.87 5.16c-7.61 10-9.28 12.89-8.18 18.05c2.23 10.37 18.86 41.24 46.19 68.51s57.31 42.85 67.72 45.07c5.38 1.15 8.33-.59 18.65-8.47c1.48-1.13 3-2.3 4.59-3.47c10.66-7.93 19.08-13.54 30.26-13.54h.06c9.73 0 18.06 4.22 31.86 11.18c18 9.08 59.11 33.59 77.14 51.78c8.77 8.84 13.66 14.48 16.05 20.81c3.6 9.53.21 17-2 22c-.37.83-.78 1.74-1.21 2.75a176.5 176.5 0 0 1-15.29 28.58c-10.63 15.9-21.4 28.21-39.38 36.58A67.4 67.4 0 0 1 391 480"/></svg>
       </button>
-      <button class="text-white bg-red-500 py-2 px-4 rounded-full" id="hangupButton">
+      <button class="px-4 py-2 text-white bg-red-500 rounded-full disabled:opacity-50" id="hangupButton">
         <svg class="w-6 h-6 rotate-[136deg]" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="currentColor" d="M391 480c-19.52 0-46.94-7.06-88-30c-49.93-28-88.55-53.85-138.21-103.38C116.91 298.77 93.61 267.79 61 208.45c-36.84-67-30.56-102.12-23.54-117.13C45.82 73.38 58.16 62.65 74.11 52a176.3 176.3 0 0 1 28.64-15.2c1-.43 1.93-.84 2.76-1.21c4.95-2.23 12.45-5.6 21.95-2c6.34 2.38 12 7.25 20.86 16c18.17 17.92 43 57.83 52.16 77.43c6.15 13.21 10.22 21.93 10.23 31.71c0 11.45-5.76 20.28-12.75 29.81c-1.31 1.79-2.61 3.5-3.87 5.16c-7.61 10-9.28 12.89-8.18 18.05c2.23 10.37 18.86 41.24 46.19 68.51s57.31 42.85 67.72 45.07c5.38 1.15 8.33-.59 18.65-8.47c1.48-1.13 3-2.3 4.59-3.47c10.66-7.93 19.08-13.54 30.26-13.54h.06c9.73 0 18.06 4.22 31.86 11.18c18 9.08 59.11 33.59 77.14 51.78c8.77 8.84 13.66 14.48 16.05 20.81c3.6 9.53.21 17-2 22c-.37.83-.78 1.74-1.21 2.75a176.5 176.5 0 0 1-15.29 28.58c-10.63 15.9-21.4 28.21-39.38 36.58A67.4 67.4 0 0 1 391 480"/></svg>
       </button>
     </div>
